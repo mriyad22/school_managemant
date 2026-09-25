1182b104dfee807e758747beeae4060fc22e4afb
mriyad22 <mriyadd22@gmail.com>
2026-09-26 01:29:04 +0600

    create a model AuthUserModel in accounts and migrate it.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AuthUserModel
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'password')
+
+admin.site.register(AuthUserModel, UserAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class AuthUserModel(AbstractUser):
+    USER_TYPE = (
+        ("Student", "Student"),
+        ("Teacher", "Teacher"),
+        ("Admin", "Admin")
+    )
+
+    user_type = models.CharField(max_length=20, choices=USER_TYPE, null=True)
+
+    def __str__(self):
+        return f"{self.username}"
